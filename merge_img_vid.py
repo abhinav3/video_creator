881dece41c129cdd9447219c25f7354974bc73a9
ABHINAV RAVI <abhinav3@users.noreply.github.com>
2026-02-15 11:50:36 +0530

    Update file paths for audio and image assets

--- merge_img_vid.py
+++ merge_img_vid.py
@@ -2,9 +2,9 @@
 from moviepy import ImageClip, AudioFileClip
 
 # Load assets
-audio = AudioFileClip("/Users/aravi2/Downloads/sambhu.mp3")
-clip = ImageClip("/Users/aravi2/Downloads/shiv_barati.png", duration=audio.duration)
+audio = AudioFileClip("/Downloads/sambhu.mp3")
+clip = ImageClip("/Downloads/shiv_barati.png", duration=audio.duration)
 
 # Set audio and write file with AAC codec
 clip = clip.with_audio(audio)
-clip.write_videofile("/Users/aravi2/Downloads/video.mp4", fps=24, audio_codec="aac") # <-- Added audio_codec
+clip.write_videofile("/Downloads/video.mp4", fps=24, audio_codec="aac") # <-- Added audio_codec
